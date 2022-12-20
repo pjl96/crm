79b1913f0a41e321e0545ddedae11c4446bf199b
“pjl96” <zakharov.oleg1996@gmail.com>
2022-12-20 19:46:28 +0300

    add main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalDiscountInput = document.querySelector('.modal__input');
+
